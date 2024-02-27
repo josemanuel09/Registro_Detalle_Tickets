c6605b04beb7be21419fdf705389582811bc98b5
José Manuel Polanco Mejia <jose_polanco13@ucne.edu.do>
2024-02-26 23:18:36 -0400

    Agregando el modelo de TicketsDetalle

---
+++ Registro_Detalle_Tickets.Shared/Models/TicketsDetalle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Registro_Detalle_Tickets.Shared.Models
+{
+    public class TicketsDetalle
+    {
+        [Key]
+        public int DetalleId { get; set; }
+        [Required(ErrorMessage = "Este campo es requerido")]
+        [ForeignKey("TicketId")]
+        public int TicketId { get; set; }
+        [Required(ErrorMessage = "Este campo es requerido")]
+        public string? Emisor { get; set; }
+        [Required(ErrorMessage = "Este campo es requerido")]
+        public string? Mensaje { get; set; }
+    }
+}
