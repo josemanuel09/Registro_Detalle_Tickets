23384346c9f7920bb492e022c4f7f7b51e3d2256
José Manuel Polanco Mejia <jose_polanco13@ucne.edu.do>
2024-02-27 01:09:00 -0400

    Modificando controller

--- Registro_Detalle_Tickets.Api/Controllers/TicketsController.cs
+++ Registro_Detalle_Tickets.Api/Controllers/TicketsController.cs
@@ -1,14 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
+﻿
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Registro_Detalle_Tickets.Api.DAL;
 using Registro_Detalle_Tickets.Shared.Models;
 
-namespace Registro_Detalle_Tickets.Api.Controllers
+
+
+
+
+namespace SistemaTicketsAssembly.Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
