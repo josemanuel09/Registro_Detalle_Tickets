bdb70570dfe2a8c2af7253772d3e63aea8782ca3
José Manuel Polanco Mejia <jose_polanco13@ucne.edu.do>
2024-02-26 23:18:17 -0400

    Agregando el modelo de Tickets

---
+++ Registro_Detalle_Tickets.Shared/Models/Tickets.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Registro_Detalle_Tickets.Shared.Models
+{
+    public class Tickets
+    {
+        [Key]
+        public int TicketId { get; set; }
+
+        [Required(ErrorMessage = "Este campo es requerido")]
+        public DateTime Fecha { get; set; } = DateTime.Now;
+
+        [Required(ErrorMessage = "Este campo es requerido")]
+        public string? Asunto { get; set; }
+
+        [Required(ErrorMessage = "Este campo es requerido")]
+        public string? Descripcion { get; set; }
+
+        [Required(ErrorMessage = "Este campo es requerido")]
+        [StringLength(30, ErrorMessage = "No puede exceder los 30 Caracteres")]
+        public string? SolicitadoPor { get; set; }
+    }
+}
