a0c8957211679f60d5c68a91b192eeb18a273f38
José Manuel Polanco Mejia <jose_polanco13@ucne.edu.do>
2024-02-26 23:21:31 -0400

    Agregando el contexto

---
+++ Registro_Detalle_Tickets.Api/DAL/Contexto.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Registro_Detalle_Tickets.Shared.Models;
+
+namespace Registro_Detalle_Tickets.Api.DAL
+{
+    public class Contexto: DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+        public DbSet<Tickets> Tickets { get; set; }
+        public DbSet<TicketsDetalle> TicketsDetalle { get; set; }
+    }
+}
